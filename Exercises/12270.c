2e80dc8be8ab95553b9bee4a528e4bdcde0de26b
Fernando Casado <casadix@gmail.com>
2015-08-07 17:15:07 -0300

    Exercises 070815

---
+++ Exercises/12270.c
@@ -0,0 +1,20 @@
+/*
+    Input two numbers and work out their sum, average and sum of the
+    squares of the numbers.
+    
+    cc 12270.c -o 12270 -lm 
+ */
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float a,b,sum;
+    printf("\nInput two numbers: ");
+    scanf("%f %f", &a, &b);
+    printf("\nSum: %.2f", a+b);
+    printf("\nAverage: %.2f", (a+b)/2.0);
+    printf("\nSum of Squares: %.2f", powf(a,2.0)+powf(b,2.0));
+    return 0;
+}
